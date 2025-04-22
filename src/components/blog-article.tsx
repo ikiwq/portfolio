87d4798a5b10774487b895ab21bf7ff61d3e0fc9
ikiwq <ikiigaiiii@gmail.com>
2025-04-22 11:52:40 +0200

    disabled blog links temporarely

--- src/components/blog-article.tsx
+++ src/components/blog-article.tsx
@@ -1,23 +1,28 @@
 "use client";
 import {type BlogArticle} from "@/api/types.api";
 import {useLocaleContext} from "@/context/locale";
-import {IconArrowRight} from "@tabler/icons-react";
+// import {IconArrowRight} from "@tabler/icons-react";
 
 export default function BlogArticleCard({article}: {article: BlogArticle}) {
   const {intl} = useLocaleContext();
   return (
     <div
       key={article.id}
-      className="flex items-center justify-between cursor-pointer blog-card"
+      // TODO: Put cursor-pointer when blog is finally working again
+      className="flex items-center justify-between blog-card"
     >
       <div className="w-full">
         <div className="flex items-center justify-between w-full">
-          <div className="flex items-center text-sm blog-link">
+
+          {/* TODO: Uncomment when blog is finally working again */}
+          <div className="flex items-center text-sm">
+            {/* <div className="flex items-center text-sm blog-link"> */}
             {article.title}
           </div>
-          <div>
+          {/* TODO: Uncomment when blog is finally working again */}
+          {/* <div>
             <IconArrowRight className="w-4 h-4 blog-icon"/>
-          </div>
+          </div> */}
         </div>
         <p className="text-neutral-400 text-xs">
           {intl.format(new Date(article.createdAt))}
